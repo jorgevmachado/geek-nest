4939107ba9b561f82c00eeaff1087e4b2b5283da
Jorge Machado <jorge.vmachado@hotmail.com>
2024-08-21 13:00:42 -0300

    Improvement in controller user unit tests

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -14,8 +14,6 @@ import { EStatus } from '@/enums/status.enum';
 import { ERole } from '@/modules/users/users.enum';
 import { Users } from './users.entity';
 
-import { UpdateUserDto } from './dto/update-user.dto';
-
 describe('UsersController', () => {
   let controller: UsersController;
   let service: UsersService;
@@ -47,68 +45,80 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should be get success', async () => {
+  it('should return users successfully with method get', async () => {
     const USER_CLEAN = userClean(USER_FIXTURE);
     jest.spyOn(service, 'findAll').mockResolvedValueOnce([USER_CLEAN]);
     const result = await controller.findAll({});
     expect(result).toEqual([USER_CLEAN]);
   });
 
-  it('should be get success with user removed', async () => {
-    const USER_CLEAN = userClean({
-      ...USER_FIXTURE,
-      status: EStatus.INACTIVE,
-      deletedAt: new Date(),
-    });
-    jest.spyOn(service, 'findAll').mockResolvedValueOnce([USER_CLEAN]);
+  it('should return all users successfully with method get', async () => {
+    const USERS_CLEAN = [
+      userClean({
+        ...USER_FIXTURE,
+        status: EStatus.INACTIVE,
+        deletedAt: new Date(),
+      }),
+    ];
+    jest.spyOn(service, 'findAll').mockResolvedValueOnce(USERS_CLEAN);
     const result = await controller.findAll({ all: true });
-    expect(result).toEqual([USER_CLEAN]);
+    expect(result).toEqual(USERS_CLEAN);
   });
 
-  it('should be get with param id success', async () => {
-    const USER_CLEAN = userClean(USER_FIXTURE);
-    jest.spyOn(service, 'findOne').mockResolvedValueOnce(USER_CLEAN);
-    const result = await controller.findOne(USER_FIXTURE, USER_CLEAN.id, false);
-    expect(result).toEqual(USER_CLEAN);
-  });
-
-  it('should be get with param id success user removed', async () => {
-    const USER_CLEAN = userClean({
+  it('should return user successfully with param id and method get', async () => {
+    const USER_CLEAN_SUCCESSFULLY = userClean({
       ...USER_FIXTURE,
+      id: 'USER_CLEAN_SUCCESSFULLY',
+    });
+    jest
+      .spyOn(service, 'findOne')
+      .mockResolvedValueOnce(USER_CLEAN_SUCCESSFULLY);
+    const result = await controller.findOne(
+      USER_FIXTURE,
+      USER_CLEAN_SUCCESSFULLY.id,
+      false,
+    );
+    expect(result).toEqual(USER_CLEAN_SUCCESSFULLY);
+  });
+
+  it('should return user removed successfully with param id and method get', async () => {
+    const USER_CLEAN_REMOVED = userClean({
+      ...USER_FIXTURE,
+      id: 'USER_CLEAN_REMOVED',
       status: EStatus.INACTIVE,
       deletedAt: new Date(),
     });
-    jest.spyOn(service, 'findOne').mockResolvedValueOnce(USER_CLEAN);
+    jest.spyOn(service, 'findOne').mockResolvedValueOnce(USER_CLEAN_REMOVED);
     const result = await controller.findOne(
       {
         ...USER_FIXTURE,
         role: ERole.ADMIN,
       },
-      USER_CLEAN.id,
-      false,
+      USER_CLEAN_REMOVED.id,
+      true,
     );
-    expect(result).toEqual(USER_CLEAN);
+    expect(result).toEqual(USER_CLEAN_REMOVED);
   });
 
-  it('should be path with param id success', async () => {
-    const updateUserDto: UpdateUserDto = {
+  it('should return update user successfully with param id and method path', async () => {
+    const USER_CLEAN_UPDATE_SUCCESSFULLY = userClean({
+      ...USER_FIXTURE,
+      id: 'USER_CLEAN_UPDATE_SUCCESSFULLY',
+    });
+    jest
+      .spyOn(service, 'update')
+      .mockResolvedValueOnce(USER_CLEAN_UPDATE_SUCCESSFULLY);
+    const result = await controller.update(USER_FIXTURE, USER_FIXTURE.id, {
       cpf: USER_FIXTURE.cpf,
       name: USER_FIXTURE.name,
       gender: USER_FIXTURE.gender,
       dateOfBirth: USER_FIXTURE.dateOfBirth,
-    };
-    const USER_CLEAN = userClean(USER_FIXTURE);
-    jest.spyOn(service, 'update').mockResolvedValueOnce(USER_CLEAN);
-    const result = await controller.update(
-      USER_FIXTURE,
-      USER_FIXTURE.id,
-      updateUserDto,
-    );
+    });
 
-    expect(result).toEqual(USER_CLEAN);
+    expect(result).toEqual(USER_CLEAN_UPDATE_SUCCESSFULLY);
   });
 
-  it('should be path promote with param id success', async () => {
+  it('should return promote user successfully with param id and method path', async () => {
     const response = {
       message: 'User promoted to administrator successfully',
     };
@@ -118,7 +128,7 @@ describe('UsersController', () => {
     expect(result).toEqual(response);
   });
 
-  it('should be delete with param id success', async () => {
+  it('should return delete user successfully with param id and method delete', async () => {
     const response = {
       message: 'User with id USER_INCOMPLETE successfully removed',
     };
